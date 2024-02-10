7a1bfd4e09bd07ffcf4ae4c0c2b651ddffa71890
Hans Geerts <Hans Geerts@DESKTOP-DVF7FH7>
2024-02-10 07:58:37 +0100

    Add project files.

---
+++ HGR.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGR.Application
+{
+    public static class DependencyInjection
+    {
+        //TODO: Add DI + mediatr etc here, ...
+        public static IServiceCollection AddApplicationLayerDependencies(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ HGR.WebUI.Server/HGR.WebUI.Server/Program.cs
@@ -0,0 +1,38 @@
+using HGR.WebUI.Server.Client.Pages;
+using HGR.WebUI.Server.Components;
+using HGR.Application;
+using HGR.Infrastructure;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddApplicationLayerDependencies();
+builder.Services.AddInfrastructureLayerDependencies();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(HGR.WebUI.Server.Client._Imports).Assembly);
+
+app.Run();
